49e373b439139f82a05c40c5647db88de82ebb0d
EmmanuelPlatinum <141779528+EmmanuelPlatinum@users.noreply.github.com>
2024-08-19 10:45:10 -0400

    Minha pagina final

--- Pagina de Duvidas/Main.js
+++ Pagina de Duvidas/Main.js
@@ -1,28 +1,34 @@
-let toggles = document.getElementsByClassName('toggle');
-let contentDiv = document.getElementsByClassName('content');
-let icons = document.getElementsByClassName('icon');
+document.addEventListener('DOMContentLoaded', function() {
+    let toggles = document.querySelectorAll('.toggle');
+    let contents = document.querySelectorAll('.content');
+    let icons = document.querySelectorAll('.icon');
 
-for(let i = 0; i < toggles.length; i++) {
-    toggles[i].addEventListener('click', () => {
-        if(parseInt(contentDiv[i].style.height) !== contentDiv[i].scrollHeight) {
-            contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
-            toggles[i].style.color = "#0084e9";
-            icons[i].classList.remove('fa-plus');
-            icons[i].classList.add('fa-minus');
-        } else {
-            contentDiv[i].style.height = "0px";
-            toggles[i].style.color = "#111130";
-            icons[i].classList.remove('fa-minus');
-            icons[i].classList.add('fa-plus');
-        }
+    toggles.forEach((toggle, index) => {
+        toggle.addEventListener('click', () => {
+            let content = contents[index];
+            let icon = icons[index];
 
-        for(let j = 0; j < contentDiv.length; j++) {
-            if(j !== i) {
-                contentDiv[j].style.height = "0px";
-                toggles[j].style.color = "#111130";
-                icons[j].classList.remove('fa-minus');
-                icons[j].classList.add('fa-plus');
+            if (content.style.height && content.style.height !== '0px') {
+                content.style.height = '0px';
+                toggle.style.color = '#000000'; // Cor após o fechamento
+                icon.classList.remove('fa-minus');
+                icon.classList.add('fa-plus');
+            } else {
+                content.style.height = content.scrollHeight + 'px';
+                toggle.style.color = '#000000'; // Cor após abrir o conteúdo
+                icon.classList.remove('fa-plus');
+                icon.classList.add('fa-minus');
             }
-        }
+
+            // Fechar outras seções
+            contents.forEach((otherContent, otherIndex) => {
+                if (otherIndex !== index) {
+                    otherContent.style.height = '0px';
+                    toggles[otherIndex].style.color = '#000000'; // Cor ao fechar outras seções
+                    icons[otherIndex].classList.remove('fa-minus');
+                    icons[otherIndex].classList.add('fa-plus');
+                }
+            });
+        });
     });
-}
+});
